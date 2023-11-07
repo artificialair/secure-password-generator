1df7e02da0f58b32e6f45b2202d7e62d3e410aee
artificialair <jaybaker964@gmail.com>
2023-11-06 19:01:21 -0500

    added password generator

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Character lists
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numericCharacters = "1234567890"
+var specialCharacters = "!\"#$%&'()*+,-./\\:;<=>?@[\]^_`{|}~"
+
+function generatePassword() {
+  var password = "";
+
+  var lengthInput = 12;
+
+  var validCharacters = lowercaseLetters;
+
+  while (password.length < lengthInput) {
+    var randomCharacter = Math.floor(Math.random() * validCharacters.length)
+    password += validCharacters[randomCharacter]
+  }
+  
+  console.log(password);
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
