498871c9fdcf3bd35d0d7f47d841b69caa28efa4
artificialair <jaybaker964@gmail.com>
2023-11-06 19:21:43 -0500

    added length prompt

--- script.js
+++ script.js
@@ -10,11 +10,38 @@ var specialCharacters = "!\"#$%&'()*+,-./\\:;<=>?@[\]^_`{|}~"
 function generatePassword() {
   var password = "";
 
-  var lengthInput = 12;
+  var passwordLength;
+  // !passwordLength returns true only if the password length is not null
+  while (!passwordLength) {
+    var lengthInput = prompt("How long would you like your password to be?");
 
-  var validCharacters = lowercaseLetters;
+    // If the cancel button is clicked, we want to cancel the password generating process.
+    if (lengthInput === null) {
+      return;
+    }
 
-  while (password.length < lengthInput) {
+    // If anything other than a number is entered, returns a unique error message. (I'm sure there are more appropriate ways to do this.)
+    if (lengthInput !== parseInt(lengthInput)) {
+      alert("You must input a valid number!")
+      continue;
+    }
+
+    // Length checks
+    if (lengthInput < 8) {
+      alert("That is too short!");
+      continue;
+    }
+    if (lengthInput > 128) {
+      alert("That is too long!");
+      continue;
+    }
+
+    passwordLength = lengthInput;
+  }
+
+  var validCharacters = "";
+
+  while (password.length < passwordLength) {
     var randomCharacter = Math.floor(Math.random() * validCharacters.length)
     password += validCharacters[randomCharacter]
   }
