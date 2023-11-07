6b72f01849a2991ffae09a5b26ec05faa444eb5f
artificialair <jaybaker964@gmail.com>
2023-11-06 19:29:38 -0500

    added character type prompts

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function generatePassword() {
     }
 
     // If anything other than a number is entered, returns a unique error message. (I'm sure there are more appropriate ways to do this.)
-    if (lengthInput !== parseInt(lengthInput)) {
+    if (lengthInput != parseInt(lengthInput)) {
       alert("You must input a valid number!")
       continue;
     }
@@ -40,13 +40,43 @@ function generatePassword() {
   }
 
   var validCharacters = "";
+  // !validCharacters returns true only if the string is empty. 
+  while (!validCharacters) {
+    var userInput = prompt("What types of characters do you want in your password? (Valid keywords: lower, upper, number, special)")
+    userInput = userInput.toLowerCase()
 
+    // If the cancel button is clicked, we want to cancel the password generating process.
+    if (lengthInput === null) {
+      return;
+    }
+
+    // Check for valid keywords and add the respective characters
+    if (userInput.includes("lower")) {
+      validCharacters += lowercaseLetters;
+    }
+    if (userInput.includes("upper")) {
+      validCharacters += uppercaseLetters;
+    }
+    if (userInput.includes("number")) {
+      validCharacters += numericCharacters;
+    }
+    if (userInput.includes("special")) {
+      validCharacters += specialCharacters;
+    }
+
+    if (!validCharacters) {
+      alert("Please input at least one valid character type.");
+      continue;
+    }
+  }
+  
+  // using the above variables, the password is created!
   while (password.length < passwordLength) {
     var randomCharacter = Math.floor(Math.random() * validCharacters.length)
     password += validCharacters[randomCharacter]
   }
   
-  console.log(password);
+  console.log("New Password Generated: " + password);
   return password;
 }
 
